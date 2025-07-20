a6e2219186517100cd316c9d2c499b3925381b19
Krak9n <alyoshagnatu@gmail.com>
2025-07-20 14:44:35 +0200

    beggining

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+extern crate glfw;
+
+use glfw::{Action, Context, Key};
+
+static WIDTH: u32 = 1500;
+static HEIGHT: u32 = 900;
+
+fn main() 
+{
+   use glfw::fail_on_errors;
+
+let mut glfw = glfw::init(fail_on_errors!()).unwrap();
+
+    // Create a windowed mode window and its OpenGL context
+    let (mut window, events) = glfw.create_window(
+        WIDTH,
+        HEIGHT,
+        "Hello this is window",
+        glfw::WindowMode::Windowed)
+    .expect("Failed to create GLFW window.");
+
+    glfw.window_hint(glfw::WindowHint::Resizable(false));    
+    
+    // Make the window's context current
+    window.make_current();
+    window.set_key_polling(true);
+
+    // Loop until the user closes the window
+    while !window.should_close() 
+    {
+        // Swap front and back buffers
+        window.swap_buffers();
+
+        // Poll for and process events
+        glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) 
+        {
+            println!("{:?}", event);
+            // switch
+            match event 
+            {
+                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                    window.set_should_close(true)
+                },
+                _ => {},
+            }
+        }
+    }
 }
+
