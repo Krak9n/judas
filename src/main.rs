2ddee59228f2eac3505efaa5e646ae54a342b518
Krak9n <alyoshagnatu@gmail.com>
2025-07-28 14:55:08 +0200

    d

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,233 @@
-#![cfg_attr(not(debug_assertions), linux_subsystem = "linux")]
-#![allow(clippy::single_match)]
+#![allow(
+    dead_code,
+    unused_variables,
+    clippy::too_many_arguments,
+    clippy::unnecessary_wraps
+)]
 
-extern crate glfw;
-extern crate glad_gl;
-// include the OpenGL type aliases
-use gl::types::*;
+extern crate sdl3;
 
-use glad_gl::gl;
-use glfw::{Action, Context, Key};
+use log::*;
 
-static TITLE: &str = "agonia";
+use imgui::*;
+
+use std::collections::HashSet;
+use std::ffi::CStr;
+use std::os::raw::c_void;
+
+use winit::keyboard::*;
+use winit::dpi::LogicalSize;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::EventLoop;
+use winit::window::{Window, WindowBuilder};
+
+use anyhow::{anyhow, Result};
+
+use vulkanalia::vk::ExtDebugUtilsExtension;
+use vulkanalia::Version;
+use vulkanalia::loader::{LibloadingLoader, LIBRARY};
+use vulkanalia::window as vk_window;
+use vulkanalia::prelude::v1_0::*;
+
+
 static WIDTH: u32 = 1500;
-static HEIGHT: u32 = 800;
+static HEIGHT: u32 = 900;
+static TITLE: &str = "judas";
 
-fn main() 
+const PORTABILITY_MAC_VERSION: Version = Version::new(1, 3, 216);
+
+const VALIDATION_ENABLED: bool =
+    cfg!(debug_assertions);
+
+const VALIDATION_LAYER: vk::ExtensionName =
+    vk::ExtensionName::from_bytes(b"VK_LAYER_KHRONOS_validation");
+
+pub fn main() -> Result<(), Box<dyn std::error::Error>>
 {
-    use glfw::fail_on_errors;
-    let mut glfw = glfw::init(fail_on_errors!()).unwrap();
+    pretty_env_logger::init();
 
-    // Create a windowed mode window and its OpenGL context
-    let (mut window, events) = glfw.create_window(
-        WIDTH, 
-        HEIGHT, 
-        TITLE,
-        glfw::WindowMode::Windowed)         
-        .expect("Failed to create GLFW window.");
-
-    // Make the window's context current
-    window.make_current();
-    window.set_key_polling(true);
+    /* WINDOW */
+    // -------------
+    let event_loop = EventLoop::new()?;
     
-    gl::load(|e| glfw.get_proc_address_raw(e) as *const std::os::raw::c_void);
+    let window = WindowBuilder::new()
+        .with_title(TITLE)
+        .with_inner_size(LogicalSize::new(WIDTH, HEIGHT))
+        .build(&event_loop)?;
+    // ----------------
+
+    let mut world = unsafe
+    { 
+        Works::create(&window).unwrap() 
+    };
+
+    let mut value = 0;
     
-    while !window.should_close() {
-        glfw.poll_events();
-        for (_, event) in glfw::flush_messages(&events) {
-            handle_window_event(&mut window, event);
+    event_loop.run(move |event, elwt| 
+    {
+        match event {
+            // Request a redraw when all events were processed.
+            Event::AboutToWait => window.request_redraw(),
+            Event::WindowEvent { event, .. } => match event {
+                // Render a frame if our Vulkan app is not being destroyed.
+                WindowEvent::RedrawRequested if !elwt.exiting() => unsafe { world.render(&window) }.unwrap(),
+                // Destroy our Vulkan app.
+                WindowEvent::CloseRequested => {
+                    elwt.exit();
+                    unsafe { world.destroy(); }
+                }
+                _ => {}
+            }
+            _ => {}
         }
+    })?;
 
-        unsafe {
-            gl::ClearColor(0.7, 0.9, 0.1, 1.0);
-            gl::Clear(gl::COLOR_BUFFER_BIT);
+    Ok(())
+}
+
+#[derive(Clone, Debug)]
+struct Works
+{
+    entry: Entry,
+    instance: Instance,
+    data: WorkData
+}
+
+impl Works
+{
+    /// Creates our Vulkan app.
+    unsafe fn create(window: &Window) -> Result<Self> 
+    {
+        let loader = LibloadingLoader::new(LIBRARY)?;
+        let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
+        let mut data = WorkData::default();
+        let instance = create_instance(window, &entry, &mut data)?;
+        Ok(Self {entry, instance, data})
+    }
+
+    /// Renders a frame for our Vulkan app.
+    unsafe fn render(&mut self, window: &Window) -> Result<()> 
+    {
+        Ok(())
+    }
+
+    /// Destroys our Vulkan app.
+    #[rustfmt::skip]
+    unsafe fn destroy(&mut self) 
+    {
+        if VALIDATION_ENABLED 
+        {
+            self.instance.destroy_debug_utils_messenger_ext(self.data.messenger, None);
         }
-
-        window.swap_buffers();
+        self.instance.destroy_instance(None);
     }
 }
 
-fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
-    match event {
-        glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-            window.set_should_close(true)
-        }
-        _ => {}
-    }
+/// The Vulkan handles and associated properties used by our Vulkan app.
+#[derive(Clone, Debug, Default)]
+struct WorkData
+{
+    messenger: vk::DebugUtilsMessengerEXT,
 }
 
+// INSTANCE
+unsafe fn create_instance(window: &Window, entry: &Entry, data: &mut WorkData) -> Result<Instance> 
+{
+    // Application Info
+    let application_info = vk::ApplicationInfo::builder()
+        .application_name(b"judas\0")
+        .application_version(vk::make_version(1, 0, 0))
+        .engine_name(b"No Engine\0")
+        .engine_version(vk::make_version(1, 0, 0))
+        .api_version(vk::make_version(1, 0, 0));
+
+
+    let available_layers = entry
+        .enumerate_instance_layer_properties()?
+        .iter()
+        .map(|l| l.layer_name)
+        .collect::<HashSet<_>>();
+
+    if VALIDATION_ENABLED && !available_layers.contains(&VALIDATION_LAYER) 
+    {
+        return Err(anyhow!("validation layer requested but not supported."));
+    }
+
+    let layers = if VALIDATION_ENABLED 
+    {
+        vec![VALIDATION_LAYER.as_ptr()]
+    } else {
+        Vec::new()
+    };
+
+    // Extensions
+    let mut extensions = vk_window::get_required_instance_extensions(window)
+        .iter()
+        .map(|e| e.as_ptr())
+        .collect::<Vec<_>>();
+    
+    // Required by Vulkan SDK on macOS 
+    let flags = if cfg!(target_os = "mac") && entry.version()? >= PORTABILITY_MAC_VERSION 
+    {
+        info!("enabling extensions for mac portability.");
+        extensions.push(vk::KHR_GET_PHYSICAL_DEVICE_PROPERTIES2_EXTENSION.name.as_ptr());
+        extensions.push(vk::KHR_PORTABILITY_ENUMERATION_EXTENSION.name.as_ptr());
+        vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
+    } else {
+        vk::InstanceCreateFlags::empty()
+    };
+
+    // Create
+    let info = vk::InstanceCreateInfo::builder()
+        .application_info(&application_info)
+        .enabled_layer_names(&layers)
+        .enabled_extension_names(&extensions)
+        .flags(flags);
+
+
+
+    let mut info = vk::InstanceCreateInfo::builder()
+        .application_info(&application_info)
+        .enabled_layer_names(&layers)
+        .enabled_extension_names(&extensions)
+        .flags(flags);
+
+    let mut debug_info = vk::DebugUtilsMessengerCreateInfoEXT::builder()
+        .message_severity(vk::DebugUtilsMessageSeverityFlagsEXT::all())
+        .message_type(
+            vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
+                | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION
+                | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE,
+        )
+        .user_callback(Some(debug_callback));
+
+    if VALIDATION_ENABLED 
+    {
+        info = info.push_next(&mut debug_info);
+    }
+
+    Ok(entry.create_instance(&info, None)?)
+}
+
+
+extern "system" fn debug_callback(
+    severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+    type_: vk::DebugUtilsMessageTypeFlagsEXT,
+    data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+    _: *mut c_void,
+) -> vk::Bool32 {
+    let data = unsafe { *data };
+    let message = unsafe { CStr::from_ptr(data.message) }.to_string_lossy();
+
+    if severity >= vk::DebugUtilsMessageSeverityFlagsEXT::ERROR {
+        error!("({:?}) {}", type_, message);
+    } else if severity >= vk::DebugUtilsMessageSeverityFlagsEXT::WARNING {
+        warn!("({:?}) {}", type_, message);
+    } else if severity >= vk::DebugUtilsMessageSeverityFlagsEXT::INFO {
+        debug!("({:?}) {}", type_, message);
+    } else {
+        trace!("({:?}) {}", type_, message);
+    }
+
+    vk::FALSE
+}
